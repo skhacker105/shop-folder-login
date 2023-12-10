fa33cfab3017d7ae2248141bd696f527f7bc250b
skhacker105 <skhacker105@gmail.com>
2023-12-10 22:14:16 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Subject } from 'rxjs';
+import { LoginPageComponent } from 'shop-folder-component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, LoginPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'shop-folder-login';
+  phoneEmailSubmitResponse = new Subject<any>();
+
+  handlePhoneEmailSubmit(phoneEmail: string) {}
+
+  handleOTPSubmit(otp: string) {}
+
 }
